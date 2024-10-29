9ef39eeb5befd179e869b00740406fb75ef20d40
Fredrik Ravndal Gaasø <112083574+fredrikgaaso@users.noreply.github.com>
2024-10-29 10:38:47 +0100

    Add files via upload

---
+++ diy-lab/src/main/java/com/DIY_Lab/diy_lab/DiyLabApplication.java
@@ -0,0 +1,13 @@
+package com.DIY_Lab.diy_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiyLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiyLabApplication.class, args);
+	}
+
+}
---
+++ diy-lab/src/test/java/com/DIY_Lab/diy_lab/DiyLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DIY_Lab.diy_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiyLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
